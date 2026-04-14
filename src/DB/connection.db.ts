f4b4725c435b50e3070033455fd8888ca5efe4f5
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-04-14 21:03:44 +0200

    feat: implement MongoDB connection utility with event listeners

--- src/DB/connection.db.ts
+++ src/DB/connection.db.ts
@@ -1,13 +1,26 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../config/config.service";
 const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("MongoDB connected");
+    });
+    mongoose.connection.on("open", () => console.log("open"));
+    mongoose.connection.on("disconnected", () => console.log("disconnected"));
+    mongoose.connection.on("reconnected", () => console.log("reconnected"));
+    mongoose.connection.on("disconnecting", () => console.log("disconnecting"));
+    mongoose.connection.on("close", () => console.log("close"));
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    mongoose.connection.on("error", (error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
